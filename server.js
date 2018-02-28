a5169e41513885e42148bbe437a38de18455701f
Jason Kunkel <jasonakunkel@outlook.com>
2018-02-27 23:09:38 -0800

    added server.js package, npm packages, and created JSON

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express"); 
+var bodyParser = require("body-parser");
+
+var port = 3000;
+
+var app = express(); 
+
+// This section gives static content for the app from the public directory
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Sets Handlebars;
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars"); 
+
+// Imports the routes and provides server access to them
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
