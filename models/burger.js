1a326184c89a25c763f22606d13ffafca84fde50
Jason Kunkel <jasonakunkel@outlook.com>
2018-02-28 21:21:18 -0800

    added model file burger.js to import ORM to interact with database

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+// Var to import ORM from ORM.js to create function that interacts with database
+var orm = require("../confi/orm.js");
+
+var burger = {
+	all: function(cb) {
+		orm.all("burgers", function(res) {
+			cb(res);
+		});
+	},
+
+	// Variable columns and values that are arrays
+	create: function(cols, vals, cb) {
+		orm.create("burgers", cols, vals, function(res) {
+			cb(res);
+		});
+	},
+
+	update: function(objColVals, condition, cb) {
+		orm.update("burgers", objColVals, condition, function(res) {
+			cb(res);
+		});
+	}
+};
+
+// Export database functions for controller (burgers_controller.js).
+module.exports = burger;
