22b77a0c0434e25f4deeeecf17a2a4b5cf57395a
Jason Kunkel <jasonakunkel@outlook.com>
2018-03-01 18:31:30 -0800

    added burgers to the table in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO burgers (burger_name) VALUES ('Royale With Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('Krabby Patty');
+INSERT INTO burgers (burger_name) VALUES ('The Heart Clogger');
