0d7847803b87b5d8e0a742a9da1ec3428dc409bf
Jason Kunkel <jasonakunkel@outlook.com>
2018-03-01 18:37:26 -0800

    added language to delete burger (if needed in future) for Models/burgers.js file

--- models/burger.js
+++ models/burger.js
@@ -2,24 +2,35 @@
 var orm = require("../config/orm.js");
 
 var burger = {
+	// Model calling on ORM to show all values in database
 	all: function(cb) {
 		orm.all("burgers", function(res) {
 			cb(res);
 		});
 	},
 
-	// Variable columns and values that are arrays
+	// Model calling ORM.js to add values to database
 	create: function(cols, vals, cb) {
 		orm.create("burgers", cols, vals, function(res) {
 			cb(res);
 		});
 	},
 
+	// Model calling ORM.js to update values in database
 	update: function(objColVals, condition, cb) {
 		orm.update("burgers", objColVals, condition, function(res) {
 			cb(res);
 		});
 	}
+
+ 	// // Model calling ORM.js to delete from the burger database - future use.
+  //   delete: function (condition, callback) {
+  //       orm.delete('burgers', condition, function (result) {
+  //           callback(result);
+  //       });
+  //   }
+
+
 };
 
 // Export database functions for controller (burgers_controller.js).
